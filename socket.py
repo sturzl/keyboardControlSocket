0c44dc5e70fe73ca3c7ff344fbbf2164e8ec44a3
Paul Nettleton <paulnett7@hotmail.com>
2016-05-17 14:03:41 -0400

    added functionality to the socket

--- socket.py
+++ socket.py
@@ -1,1 +1,18 @@
+class ServerSocket:
 
+    def initializeSocket(self, sock=None):
+        if sock is None:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
+
+    def connect(self, host, port):
+        self.sock.connect((host, port))
+
+    def sendCommand(self, msg):
+        totalsent = 0
+        while totalsent < MSGLEN:
+            sent = self.sock.send(msg[totalsent:])
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
